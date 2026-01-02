d278088165f852268136cee0763c69441bedac90
birukindrias <alltechs64@gmail.com>
2026-01-02 07:13:06 -0800

    product request

---
+++ server.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Laravel - A PHP Framework For Web Artisans
+ *
+ * @package  Laravel
+ * @author   Taylor Otwell <taylor@laravel.com>
+ */
+
+$uri = urldecode(
+    parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
+);
+
+// This file allows us to emulate Apache's "mod_rewrite" functionality from the
+// built-in PHP web server. This provides a convenient way to test a Laravel
+// application without having installed a "real" web server software here.
+if ($uri !== '/' && file_exists(__DIR__.'/public'.$uri)) {
+    $file = __DIR__.'/public'.$uri;
+    $extension = pathinfo($file, PATHINFO_EXTENSION);
+    $mimes = [
+        'css' => 'text/css',
+        'js'  => 'application/javascript',
+        'png' => 'image/png',
+        'jpg' => 'image/jpeg',
+        'jpeg'=> 'image/jpeg',
+        'gif' => 'image/gif',
+        'svg' => 'image/svg+xml',
+        'ico' => 'image/x-icon',
+        'woff' => 'font/woff',
+        'woff2'=> 'font/woff2',
+        'ttf' => 'font/ttf',
+        'otf' => 'font/otf',
+    ];
+
+    if (isset($mimes[$extension])) {
+        header('Content-Type: ' . $mimes[$extension]);
+    }
+
+    readfile($file);
+    exit;
+}
+
+require_once __DIR__.'/public/index.php';
